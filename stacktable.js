8bfdf015798c538fa023e37845a3e1766c037409
Bryan Buchs <bryan@bryanbuchs.com>
2013-12-05 12:49:04 -0600

    store classnames applied to rows and cells as settings/options

--- stacktable.js
+++ stacktable.js
@@ -13,19 +13,25 @@
 
   $.fn.stacktable = function(options) {
     var $tables = this,
-        defaults = {id:'stacktable',hideOriginal:false},
+        defaults = {
+          classname: 'stacktable',
+          hideOriginal: false,
+          title: 'st-title',
+          key: 'st-key',
+          val: 'st-val'
+        },
         settings = $.extend({}, defaults, options);
 
     return $tables.each(function() {
-      var $stacktable = $('<table class="'+ settings.id +'"></table>');
+      var $stacktable = $('<table class="'+ settings.classname +'"></table>');
       if (typeof settings.myClass !== undefined) $stacktable.addClass(settings.myClass);
       var markup = '';
       $table = $(this);
       $topRow = $table.find('tr').eq(0);
       $table.find('tr').each(function(index,value) {
-        // for the first row, top left table cell is the head of the table
+        // for the first row, top left table cell is the head ("title") of the table
         if (index===0) {
-          markup += '<thead><tr><th class="st-head-row st-head-row-main" colspan="2">'+$(this).find('th,td').eq(0).html()+'</th></tr></thead>';
+          markup += '<thead><tr><th class="'+ settings.title +'" colspan="2">'+$(this).find('th,td').eq(0).html()+'</th></tr></thead>';
         }
         // for the other rows, put the left table cell as the head for that row
         // then iterate through the key/values
@@ -33,16 +39,16 @@
           markup += '<tbody>';
           $(this).find('td').each(function(index,value) {
             if (index===0) {
-              markup += '<tr><th class="st-head-row" colspan="2">'+$(this).html()+'</th></tr>';
+              markup += '<tr><td colspan="2">'+$(this).html()+'</td></tr>';
             } else {
               if ($(this).html() !== ''){
                 markup += '<tr>';
                 if ($topRow.find('td,th').eq(index).html()){
-                  markup += '<td class="st-key">'+$topRow.find('td,th').eq(index).html()+'</td>';
+                  markup += '<td class="'+ settings.key +'">'+$topRow.find('td,th').eq(index).html()+'</td>';
                 } else {
-                  markup += '<td class="st-key"></td>';
+                  markup += '<td class="'+ settings.key +'"></td>';
                 }
-                markup += '<td class="st-val">'+$(this).html()+'</td>';
+                markup += '<td class="'+ settings.val +'">'+$(this).html()+'</td>';
                 markup += '</tr>';
               }
             }
