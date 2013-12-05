62d691e98f0c1fa58e23d68e3a3497f574b7247f
Bryan Buchs <bryan@bryanbuchs.com>
2013-12-05 13:06:07 -0600

    make jsHint happy

--- stacktable.js
+++ stacktable.js
@@ -23,32 +23,31 @@
         settings = $.extend({}, defaults, options);
 
     return $tables.each(function() {
-      var $stacktable = $('<table class="'+ settings.classname +'"></table>');
-      if (typeof settings.myClass !== undefined) $stacktable.addClass(settings.myClass);
-      var markup = '';
-      $table = $(this);
-      $topRow = $table.find('tr').eq(0);
-      $table.find('tr').each(function(index,value) {
+      var $stacktable = $('<table class="' + settings.classname + '"></table>'),
+          markup = '',
+          $table = $(this),
+          $topRow = $table.find('tr').eq(0);
+      $table.find('tr').each(function(index) {
         // for the first row, top left table cell is the head ("title") of the table
         if (index===0) {
-          markup += '<thead><tr><th class="'+ settings.title +'" colspan="2">'+$(this).find('th,td').eq(0).html()+'</th></tr></thead>';
+          markup += '<thead><tr><th class="' + settings.title + '" colspan="2">'+$(this).find('th,td').eq(0).html()+'</th></tr></thead>';
         }
         // for the other rows, put the left table cell as the head for that row
         // then iterate through the key/values
         else {
           markup += '<tbody>';
-          $(this).find('td').each(function(index,value) {
+          $(this).find('td').each(function(index) {
             if (index===0) {
-              markup += '<tr><td colspan="2">'+$(this).html()+'</td></tr>';
+              markup += '<tr><td colspan="2">' + $(this).html() + '</td></tr>';
             } else {
               if ($(this).html() !== ''){
                 markup += '<tr>';
                 if ($topRow.find('td,th').eq(index).html()){
-                  markup += '<td class="'+ settings.key +'">'+$topRow.find('td,th').eq(index).html()+'</td>';
+                  markup += '<td class="' + settings.key +'">' + $topRow.find('td,th').eq(index).html() + '</td>';
                 } else {
-                  markup += '<td class="'+ settings.key +'"></td>';
+                  markup += '<td class="' + settings.key + '"></td>';
                 }
-                markup += '<td class="'+ settings.val +'">'+$(this).html()+'</td>';
+                markup += '<td class="'+ settings.val +'">' + $(this).html() + '</td>';
                 markup += '</tr>';
               }
             }
@@ -56,10 +55,12 @@
           markup += '</tbody>';
         }
       });
-      $stacktable.append($(markup));
-      $table.before($stacktable);
-      if (settings.hideOriginal) $table.hide();
-    });
-  };
+    $stacktable.append($(markup));
+    $table.before($stacktable);
+    if (settings.hideOriginal) {
+      $table.hide();
+    }
+  });
+};
 
 }(jQuery));
