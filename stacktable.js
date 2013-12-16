3dd3561a8385bc02ed86ff0eefa2572f70323678
Bryan Buchs <bryan@bryanbuchs.com>
2013-12-16 12:27:33 -0600

    Don't assume the the first header cell is the table "title". Treat all columns equally as key/val pairs.

--- stacktable.js
+++ stacktable.js
@@ -9,58 +9,46 @@
  * jQuery plugin for stacking tables on small screens
  *
  */
-;(function($) {
+(function ($) {
 
-  $.fn.stacktable = function(options) {
+  $.fn.stacktable = function (options) {
     var $tables = this,
-        defaults = {
-          classname: 'stacktable',
-          hideOriginal: false,
-          title: 'st-title',
-          key: 'st-key',
-          val: 'st-val'
-        },
-        settings = $.extend({}, defaults, options);
+      defaults = {
+        classname: 'stacktable',
+        hideOriginal: false,
+        title: 'st-title',
+        key: 'st-key',
+        val: 'st-val'
+      },
+      settings = $.extend({}, defaults, options);
 
-    return $tables.each(function() {
+    return $tables.each(function () {
       var $stacktable = $('<table class="' + settings.classname + '"></table>'),
-          markup = '',
-          $table = $(this),
-          $topRow = $table.find('tr').eq(0);
-      $table.find('tr').each(function(index) {
-        // for the first row, top left table cell is the head ("title") of the table
-        if (index===0) {
-          markup += '<thead><tr><th class="' + settings.title + '" colspan="2">'+$(this).find('th,td').eq(0).html()+'</th></tr></thead>';
-        }
-        // for the other rows, put the left table cell as the head for that row
-        // then iterate through the key/values
-        else {
-          markup += '<tbody>';
-          $(this).find('td').each(function(index) {
-            if (index===0) {
-              markup += '<tr><td colspan="2">' + $(this).html() + '</td></tr>';
+        markup = '',
+        $table = $(this),
+        $topRow = $table.find('tr').eq(0);
+
+      $table.find('tr').each(function (index) {
+        markup += '<tbody>';
+        $(this).find('td').each(function (index) {
+          if ($(this).html() !== '') {
+            markup += '<tr>';
+            if ($topRow.find('td,th').eq(index).html()) {
+              markup += '<td class="' + settings.key + '">' + $topRow.find('td,th').eq(index).html() + '</td>';
             } else {
-              if ($(this).html() !== ''){
-                markup += '<tr>';
-                if ($topRow.find('td,th').eq(index).html()){
-                  markup += '<td class="' + settings.key +'">' + $topRow.find('td,th').eq(index).html() + '</td>';
-                } else {
-                  markup += '<td class="' + settings.key + '"></td>';
-                }
-                markup += '<td class="'+ settings.val +'">' + $(this).html() + '</td>';
-                markup += '</tr>';
-              }
+              markup += '<td class="' + settings.key + '"></td>';
             }
-          });
-          markup += '</tbody>';
-        }
+            markup += '<td class="' + settings.val + '">' + $(this).html() + '</td>';
+            markup += '</tr>';
+          }
+        });
+        markup += '</tbody>';
       });
-    $stacktable.append($(markup));
-    $table.before($stacktable);
-    if (settings.hideOriginal) {
-      $table.hide();
-    }
-  });
-};
+
+      $stacktable.append($(markup));
+      $table.before($stacktable);
+
+    });
+  };
 
 }(jQuery));
